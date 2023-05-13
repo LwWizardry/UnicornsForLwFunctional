bf2e58f37e33e168c7aae65bc3d97de538ebeb2f
Ecconia <public-commit@ecconia.com>
2023-05-13 17:42:40 +0200

    Throw exceptions for PHP warnings
    Warnings corrupt the output of 200 responses.
    If a warning occurs, it should be sent as exceptions, as warnings may never happen normally.

--- public/index.php
+++ public/index.php
@@ -14,6 +14,21 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'PRODUCTION_FRONTEND_URL'])->notEmpty();
 
+set_error_handler(function(
+	int $errno,
+	string $errstr,
+	string $errfile,
+	int $errline,
+) {
+	if($errno == E_WARNING) {
+		throw new ErrorException(
+			'Warning was raised in ' . $errfile . ' line ' . $errline . ' with message: ' . $errstr
+		);
+	}
+	
+	return false;
+});
+
 SlimSetup::setup();
 //Any error above this point will result in a CORS issue in the browser. Mostly these are failed setup errors though.
 HandlerList::initializeRouteHandlers();
